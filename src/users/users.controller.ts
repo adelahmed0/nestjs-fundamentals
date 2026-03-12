bfaeee194f6bc85269efe5baf7d77df717afef3d
Adel Ahmed <adel15952@gmail.com>
2026-03-12 03:00:42 +0200

    Add DELETE and PATCH endpoints to UsersController, rename findAll to find, add findOne endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,29 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get()
-  findAll(): string[] {
+  find(): string[] {
     return ['user1', 'user2', 'user3'];
+  }
+
+  @Get(':id')
+  findOne(): string {
+    return `This action returns a one user`;
   }
 
   @Post()
   create(): string {
     return 'This action adds a new user';
+  }
+
+  @Delete(':id')
+  remove(): string {
+    return 'This action removes a #id user';
+  }
+
+  @Patch(':id')
+  update(): string {
+    return 'This action updates a #id user';
   }
 }
