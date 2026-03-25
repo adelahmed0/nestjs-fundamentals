f258e5dd236c7a4e56bc6e48180017defb94b121
Adel Ahmed <adel15952@gmail.com>
2026-03-25 22:08:37 +0200

    Add custom error messages to username and email validation decorators in CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -2,10 +2,10 @@ import { IsEmail, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
-  @Length(3, 20)
+  @Length(3, 20, { message: 'Username must be between 3 and 20 characters' })
   readonly username: string;
 
-  @IsEmail()
+  @IsEmail({}, { message: 'Invalid email address' })
   readonly email: string;
 
   @IsString()
