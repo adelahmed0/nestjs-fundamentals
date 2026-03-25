8cef3ba291689c2e3dc35c02db793449eba91ec4
Adel Ahmed <adel15952@gmail.com>
2026-03-25 17:01:43 +0200

    Add global ValidationPipe with custom error formatting and validation decorators to CreateUserDto

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      stopAtFirstError: true,
+      exceptionFactory: (validationErrors) => {
+        const errors = {};
+        validationErrors.forEach((err) => {
+          errors[err.property] = Object.values(err.constraints ?? {})[0];
+        });
+        return new BadRequestException({
+          statusCode: 400,
+          message: 'Validation Failed',
+          errors: errors,
+        });
+      },
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
