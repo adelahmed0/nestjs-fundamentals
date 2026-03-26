3c9ab42c5b963300c0c0ee30512a8ec49296438f
Adel Ahmed <adel15952@gmail.com>
2026-03-26 16:27:33 +0200

    Implement CRUD operations in UserService with in-memory storage, add findUsers, findUserById, createUser, updateUser, and deleteUser methods using UserEntity and DTOs with UUID generation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,43 @@
+import { UpdateUserDto } from './dto/update-user.dto';
 import { Injectable } from '@nestjs/common';
+import { UserEntity } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users: UserEntity[] = [];
+
+  findUsers(): UserEntity[] {
+    return this.users;
+  }
+
+  findUserById(id: string): UserEntity {
+    return this.users.find((user) => user.id === id);
+  }
+
+  createUser(createUserDto: CreateUserDto): UserEntity {
+    const newUser: UserEntity = {
+      id: uuid(),
+      ...createUserDto,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  updateUser(id: string, updateUserDto: UpdateUserDto): UserEntity {
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index === -1) {
+      throw new Error('User not found');
+    }
+    this.users[index] = {
+      ...this.users[index],
+      ...updateUserDto,
+    };
+    return this.users[index];
+  }
+
+  deleteUser(id: string): void {
+    this.users = this.users.filter((user) => user.id !== id);
+  }
+}
