c2dfe81f274212648577d2f56759ff2692178453
Adel Ahmed <adel15952@gmail.com>
2026-03-31 00:23:04 +0200

    Implement TimeoutInterceptor with 3000ms timeout using RxJS pipe with catchError to throw RequestTimeoutException on TimeoutError and register it as global interceptor in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { WrapDataInterceptor } from './common/interceptors/wrap-data.interceptor';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -28,7 +29,10 @@ async function bootstrap() {
       },
     }),
   );
-  app.useGlobalInterceptors(new WrapDataInterceptor());
+  app.useGlobalInterceptors(
+    new WrapDataInterceptor(),
+    new TimeoutInterceptor(),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
